4e043ec40666c687e6776d37f4cff5973cd271de
whtpawsiteanon <167923204+whtpawsiteanon@users.noreply.github.com>
2024-04-25 20:23:51 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let click = document.querySelector('#clicks')
 function incrementClick() {
     click.innerHTML = parseFloat(click.innerHTML) + 1
 }
+
+document.getElementById('myButton').addEventListener('click', function() {
+    document.getElementById('myText').style.display = 'block';
+    setTimeout(function() {
+      document.getElementById('myText').style.display = 'none';
+    }, 300);
+});
