36633cfb068ec90e5e0c9f4eb6a11459f47200c8
whtpawsiteanon <167923204+whtpawsiteanon@users.noreply.github.com>
2024-05-28 01:27:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 let clicks = 0;
 let autoSaveInterval;
+let energy = 1500;
+let score = 0;
+let energyRechargeRate = 1; // энергия восстанавливается на 1 единицу каждую секунду
+let energyMax = 1500;
+let clickValue = 1; // значение одного клика
 
+document.getElementById('clicker').addEventListener('click', () => {
+  if (energy >= clickValue) {
+      energy -= clickValue;
+      score += clickValue;
+      updateEnergyBar();
+      updateScoreText();
+  }
+});
+
+(function(){
+  var interval = 10; // adjust the interval to your liking (in milliseconds)
+  var clicks = 100; // adjust the number of clicks to your liking
+  var clickElement = document.querySelector('#clickableElement'); // replace with the actual element you want to click
+
+  function autoClick() {
+    for (var i = 0; i < clicks; i++) {
+      clickElement.click();
+    }
+  }
+
+  setInterval(autoClick, interval);
+})();
+
+setInterval(() => {
+  energy = Math.min(energy + energyRechargeRate, energyMax);
+  updateEnergyBar();
+}, 1000);
+
+function updateEnergyBar() {
+  const energyFill = document.getElementById('energy-fill');
+  energyFill.style.width = `${(energy / energyMax) * 100}%`;
+  document.getElementById('energy-text').innerText = `${energy}/${energyMax}`;
+}
 
 if (/Mobi/.test(navigator.userAgent)) {
   // Если пользовательский агент содержит "Mobi", то это мобильное устройство
   // Ничего не делаем, так как вход разрешен
 } else {
   // Если пользовательский агент не содержит "Mobi", то это не мобильное устройство
-  window.location.href = 'dekstop.html';
-  alert("Вход возможен только с телефона");
+  // window.location.href = 'dekstop.html';
   // Вы можете также перенаправить пользователя на другую страницу или выполнить другие действия
 }
 
@@ -24,7 +61,7 @@ if (/Mobi/.test(navigator.userAgent)) {
 // }, 100);
 
 // Add event listeners
-document.getElementById('clicker').addEventListener('click', () => {
+document.getElementById('clicker', 'clicker2', 'clicker3', 'clicker4').addEventListener('click', () => {
     clicks++;
     document.getElementById('clicks').innerText = clicks;
 });
